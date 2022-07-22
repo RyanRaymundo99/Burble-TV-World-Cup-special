6fc3c08595cfc0bc2bade6441d0ed7dd8e590616
Ryan Raymundo <90113061+RyanRaymundo99@users.noreply.github.com>
2022-07-21 20:01:11 -0700

    enter

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,13 +12,13 @@ function App() {
         <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 lg:pr-20">
             <img class="object-cover object-center rounded-2xl" alt="hero" src={Appimg}/>
         </div>
-          <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center ">
             <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-200">Assista <span className='text-green-700'>Onde Quiser</span> </h1>
             <span></span>
             <p class="mb-8 leading-relaxed text-gray-200">A Burble TV oferece um aplicativo totalmente exclusivo para você. Basta baixa-lo e assistir tudo o que quiser e quando quiser. Nosso App oferece todos os tipos de Séries, basta escolher qual quer assistir.</p>
-            <div class="flex lg:flex-row md:flex-col text-gray-300">
+            <div class="flex lg:flex-row sm:flex-col text-gray-300 apps-download-options">
               <a href="https://play.google.com/store/apps/details?id=com.nathnetwork.xciptv">
-                <button class="bg-gray-300 bg-opacity-20 inline-flex py-3 px-5 rounded-lg items-center hover:bg-gray-800 hover:text-white focus:outline-none mr-2">
+                <button class="bg-gray-300 bg-opacity-20 inline-flex py-3 px-5 rounded-lg items-center hover:bg-gray-800 hover:text-white focus:outline-none mr-2 mb-6">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="w-6 text-gray-100 h-6" viewBox="0 0 512 512">
                   <path d="M99.617 8.057a50.191 50.191 0 00-38.815-6.713l230.932 230.933 74.846-74.846L99.617 8.057zM32.139 20.116c-6.441 8.563-10.148 19.077-10.148 30.199v411.358c0 11.123 3.708 21.636 10.148 30.199l235.877-235.877L32.139 20.116zM464.261 212.087l-67.266-37.637-81.544 81.544 81.548 81.548 67.273-37.64c16.117-9.03 25.738-25.442 25.738-43.908s-9.621-34.877-25.749-43.907zM291.733 279.711L60.815 510.629c3.786.891 7.639 1.371 11.492 1.371a50.275 50.275 0 0027.31-8.07l266.965-149.372-74.849-74.847z"></path>
                 </svg>
@@ -28,7 +28,7 @@ function App() {
                 </span>
                 </button>
               </a>
-              <a href="https://play.google.com/store/apps/details?id=com.nathnetwork.xciptv"><button class="bg-gray-300 bg-opacity-20 inline-flex py-3 px-5 rounded-lg items-center hover:bg-gray-800 hover:text-white focus:outline-none mr-2">
+              <a href="https://play.google.com/store/apps/details?id=com.nathnetwork.xciptv"><button class="bg-gray-300 bg-opacity-20 inline-flex py-3 px-5 rounded-lg items-center hover:bg-gray-800 hover:text-white focus:outline-none mr-2 mb-6">
                 <AiFillWindows class="w-8 text-gray-100 h-8" />
                 <span class="ml-4 flex items-start flex-col leading-none">
                   <span class="text-xs text-gray-100 mb-1">BAIXE NA</span>
